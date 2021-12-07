352a95850512e66df9ffff17651497cfcc9a5a0c
Peter <pete225848@gmail.com>
2021-12-07 08:58:01 -0800

    debugged

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import BookList from './views/Books/BookList'
 import BookDetail from './views/Books/BookDetail'
 import './App.css'
@@ -6,8 +7,12 @@ function App() {
   // TODO: Add routes to books & views
   return (
     <main className="container">
-      <h1>Library Catalog</h1>
-      <BookList />
+      <Router>
+        <Switch>
+          <Route path="/books" component={BookList} />
+          <Route path="/books/:id" component={BookDetail} />
+        </Switch>
+      </Router>
     </main>
   )
 }
